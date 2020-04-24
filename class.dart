4c6f93761919bd422c5c227a24b638fca6f97fd5
felixyzhang <felixyzhang@tencent.com>
2020-04-24 11:23:42 +0800

    继承、接口

--- class.dart
+++ class.dart
@@ -1,15 +1,48 @@
+
+
 class Point {
   num x;
   num y;
-  int z;
+  String name;
   static num sValue;
   //类似c++的初始化列表
-  Point(this.x, this.y):z=0;
+  Point(this.x, this.y):name="";
   Point.bottom(num x):this(x,0);//重定向构造函数
 
 
-  printInfo() => print("x=$x,y=$y,z=$z");
+  printInfo() => print("x=$x,y=$y,name=$name");
   static printStatic() => print("static value=$sValue");
+}
+
+//继承
+class Vector extends Point{
+  num z=0;
+  Vector(num x, num y,this.z) : super(x, y);
+
+  @override
+  void printInfo() => print('(x=$x,y=$y,z=$z)');
+}
+
+//接口实现
+class Coordinate implements Point{
+
+  Coordinate(this.x, this.y):name="";
+
+  @override
+  String name;
+
+  @override
+  num x;
+
+  @override
+  num y;
+
+  @override
+  printInfo() {
+    print("Coordinate:x=$x,y=$y");
+    return null;
+  }
+
 }
 
 main() {
@@ -22,5 +55,14 @@ main() {
 
   var p2 = Point.bottom(10);
   p2.printInfo();
+
+  Vector v = new Vector(100,200,300);
+  v.printInfo();
+
+  Coordinate coordinate = Coordinate(1,2);
+  coordinate.printInfo();
+
+  print(v is Point);
+  print(coordinate is Point);
 }
 
