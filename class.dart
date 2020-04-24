5d2ba1cbf0a9de042fea0000ce6eb8eeafbf3b81
felixyzhang <felixyzhang@tencent.com>
2020-04-24 11:08:36 +0800

    命名构造函数、初始化列表

--- class.dart
+++ class.dart
@@ -1,25 +1,26 @@
 class Point {
   num x;
   num y;
+  int z;
   static num sValue;
-  /**
-   * 语法糖，等价于
-      Point(num x,num y){
-      this.x=x;
-      this.y=y;
-      }
-   */
-  Point(this.x, this.y);
+  //类似c++的初始化列表
+  Point(this.x, this.y):z=0;
+  Point.bottom(num x):this(x,0);//重定向构造函数
 
-  printInfo() => print("x=$x,y=$y");
+
+  printInfo() => print("x=$x,y=$y,z=$z");
   static printStatic() => print("static value=$sValue");
 }
 
-main(){
+main() {
   Point p = Point(1, 2);
   p.printInfo();
   Point.printStatic();
 
   Point.sValue = 1;
   Point.printStatic();
+
+  var p2 = Point.bottom(10);
+  p2.printInfo();
 }
+
